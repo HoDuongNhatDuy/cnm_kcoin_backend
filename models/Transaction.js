02a789b8b2a2421c9191bb8cd300ae2b35cac1b8
Jake <forte2610@hotmail.com>
2017-12-29 11:11:50 +0700

    Added model for remote transaction

--- models/Transaction.js
+++ models/Transaction.js
@@ -5,9 +5,21 @@ let LocalTransactionSchema = new mongoose.Schema(
         src_address: {type: String, required: true},
         dest_address: {type: String, required: true},
         amount: {type: Number},
-        remaining_amount: {type: String},
+        remaining_amount: {type: Number},
         status: {type: Number}
     }
 );
 
-let LocalTransaction = module.exports = mongoose.model('LocalTransaction', LocalTransactionSchema, 'local_transactions');
+let LocalTransaction = module.exports = mongoose.model('LocalTransaction', LocalTransactionSchema, 'local-transactions');
+
+let RemoteTransactionSchema = new mongoose.Schema(
+    {
+        src_hash: {type: String},
+        index: {type: Number},
+        dst_addr: {type: String},
+        amount: {type: Number},
+        status: {type: String}
+    }
+);
+
+let RemoteTransaction = module.exports = mongoose.model('RemoteTransaction', RemoteTransactionSchema, 'remote-transactions');
